a245854a0916f41302234c9fe20fa3457c16bc3a
inpines <inpines@gmail.com>
2020-03-24 20:39:24 +0800

    maven env.

--- src/org/dotspace/creation/functional/Predicates.java
+++ src/org/dotspace/creation/functional/Predicates.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Predicate;
 
-import org.junit.platform.commons.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class Predicates {
 
