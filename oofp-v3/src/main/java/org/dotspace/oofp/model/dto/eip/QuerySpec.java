cd4334c6a2ee22df582ca52bdde2bd024eff2dcf
IISI-2502020 <jim.yeh@iisigroup.com>
2026-01-27 18:25:02 +0800

    QuerySpec 的 intoAttr 改為 AttrKey<QuerySpecParams>

--- oofp-v3/src/main/java/org/dotspace/oofp/model/dto/eip/QuerySpec.java
+++ oofp-v3/src/main/java/org/dotspace/oofp/model/dto/eip/QuerySpec.java
@@ -1,9 +1,11 @@
 package org.dotspace.oofp.model.dto.eip;
 
+import org.dotspace.oofp.utils.eip.AttrKey;
+
 public record QuerySpec(
-        String type,                 // "db" | "http" | "config"...
-        String name,                 // 白名單 key
+        String type, // "db" | "http" | "config"...
+        String name, // 白名單 key
         QuerySpecParams params,  // 純值
-        String intoAttr              // 要寫回哪個 attribute)
+        AttrKey<QuerySpecParams> intoAttr // 要寫回哪個 attribute
 ) {
 }
